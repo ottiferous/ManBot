1950eca553ce125d405d793d236fb45557cecb21
Andrew Marrone <github@andrewmarrone.com>
2012-02-25 16:43:30 -0800

    pickarea clean

--- A4Abot.rb
+++ A4Abot.rb
@@ -23,15 +23,6 @@ class ManBot
     @page = @agent.page.link_with(:text => /California/).click
   end
 
-  #    Selects area to Troll
-  #    1.
-  #       Select from a pre-defined list of places works best
-  #       if there is only a limited number of people using this program
-  #       and if they don't make a change to anything.
-  #    2.
-  #       Select from a list generated each time you log-in based on the
-  #       structure already built into the selection pages. More input but
-  #       much more stable. Usefull for people of different areas.
   def pickarea
     puts 'Getting List of locations in California.'
     list = []
@@ -51,7 +42,7 @@ class ManBot
 
   def stalk
 
-    @page = @page.link_with(:href => %r{/?p=}).click
+    @page = @page.link_with(:href => /\/?p=/).click
     
     begin
       loop do
