05141b4be2d13b045d56b233e199c1d17332d29d
Andrew Marrone <github@andrewmarrone.com>
2012-02-27 21:59:59 -0800

    search functionality is a go

--- A4Abot.rb
+++ A4Abot.rb
@@ -15,18 +15,10 @@ class ManBot
     @page = @agent.submit form
   end
 
-  def menuselect
-    @page = @agent.page.link_with(:text => 'Members Online').click
-    self.pickarea
-    @page = @agent.page.link_with(:text => /California/).click
-  end
-
   def pickarea
-    puts 'Choose your area in California:'
     list = []
-    @page = @agent.get 'http://m.adam4adam.com/index.php?section=130&area_id=569'
-    Array(@page.links_with(:dom_class => /level3/)).each do |item|
-      list << item.text.strip
+    Array(@page.links_with(:href => /area_id/)).each do |item|
+      list << item.text.strip if item.text.strip != ""
     end
     x = 1
     list.each do |place|
@@ -34,15 +26,29 @@ class ManBot
       x += 1
     end
     while true
-    puts "\nWhich location do you want to stalk?"
+      puts "\nWhich location do you want to stalk?"
       choice = STDIN.gets.chomp
       break if (1..list.length).include? choice.to_i 
       puts "That is not a valid choice. Try again."
     end
-    @page = @agent.page.link_with(:text => /#{list[choice.to_i-1]}/).click
+    @page.link_with(:text => /#{list[choice.to_i-1]}/).href
   end
 
-  def stalk
+  def search
+    @page = @agent.get 'http://m.adam4adam.com/?section=132'
+    
+    system("clear")
+    printf 'Search for : '
+    location = STDIN.gets.chomp
+    form = @page.forms.first
+    form.area_search = location
+    @page = @agent.submit form
+    self.pickarea.match(/&area_id_expand=([0-9]+)/)[1]
+    
+  end
+
+  def stalk(url)
+    @page = @agent.get url
     @page = @page.link_with(:href => /\/?p=/).click
     x = 0 
     begin
@@ -57,6 +63,8 @@ class ManBot
   end
 end
 
+home = 'http://m.adam4adam.com/index.php?section=100&mode=online&area_id='
+
 system("clear")
 printf "Username: "
 name = STDIN.gets.chomp
@@ -67,7 +75,7 @@ page = ManBot.new
 page.login(name, pass)
 system ("clear")
 puts 'Logged in...'
-page.menuselect
+a_id = page.search
 sleep(0.2)
 system("clear")
-page.stalk
+page.stalk(home + a_id)
